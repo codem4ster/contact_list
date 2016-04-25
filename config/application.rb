130c8a7539c04a4a8fac56c7408877277c6f28a5
Onur Eren Elibol <onurelibol@gmail.com>
2016-04-25 12:03:51 +0300

    swagger ile uyumlu çalışması için Rack::Cors eklendi

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,12 @@ module ContactList
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*.codemaniac.net'
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :patch, :put, :delete]
+      end
+    end
   end
 end
